266f9114f8c2864910f1f69fad03b974d597b20b
Flavia Rocha <flaviaccruzr@gmail.com>
2020-06-07 15:21:09 -0300

    Implements RNPickerSelect

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Feather as Icon } from "@expo/vector-icons";
 import { StyleSheet, ImageBackground, View, Image, Text } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import RNPickerSelect from "react-native-picker-select";
+import axios from "axios";
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
 
 const Home = () => {
   const navigation = useNavigation();
+  const [uf, setUf] = useState<string[]>([]);
+  const [city, setCity] = useState<string[]>([]);
 
-  function handleNavigateToPoints(){
-    navigation.navigate('Points')
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+        setUf(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === "0") {
+      return;
+    }
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const cityName = response.data.map((city) => city.nome);
+        setCity(cityName);
+      });
+  }, [selectedUf]);
+
+  function handleNavigateToPoints() {
+    navigation.navigate("Points");
+  }
+
+  function handleSelectUf(uf: string) {
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(city: string) {
+    setSelectedCity(city);
   }
 
   return (
@@ -24,15 +72,31 @@ const Home = () => {
           Ajudamos pessoas à encontrarem pontos de coleta de forma eficiente.
         </Text>
       </View>
-      <View style={styles.footer}></View>
-      <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-        <View style={styles.buttonIcon}>
-          <Text>
-            <Icon name="arrow-right" color="#fff" size={24} />
-          </Text>
-        </View>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </RectButton>
+      <View style={styles.footer}>
+        <RNPickerSelect
+          onValueChange={(value) => handleSelectUf(value)}
+          items={uf.map((uf) => ({
+            label: uf,
+            value: uf,
+          }))}
+        />
+
+        <RNPickerSelect
+          onValueChange={(value) => handleSelectCity(value)}
+          items={city.map((city) => ({
+            label: city,
+            value: city,
+          }))}
+        />
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#fff" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
     </ImageBackground>
   );
 };
